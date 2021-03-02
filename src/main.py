c10678e3e4b716d48e76cecd53ea6829b3df3647
Khaled ElMorshedy <elmoiv@yahoo.com>
2021-03-02 10:32:02 +0200

    Changed Version to 1.1

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ from bridge import ScanThread
 class ElmoCut(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.version = 1.0
+        self.version = 1.1
         self.icon = self.processIcon(app_icon)
 
         # Add window icon
