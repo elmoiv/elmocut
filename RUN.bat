0efc4ca86ac715965f7359e4097125f37bf25aec
Khaled ElMorshedy <elmoiv@yahoo.com>
2021-05-19 18:37:42 +0200

    Update About UI

--- RUN.bat
+++ RUN.bat
@@ -6,6 +6,7 @@ set "src=%cd%\src\"
 
 echo Updating UIs
 pyuic5 "%exe%ui_main.ui" -o "%src%ui_main.py"
+pyuic5 "%exe%ui_about.ui" -o "%src%ui_about.py"
 pyuic5 "%exe%ui_settings.ui" -o "%src%ui_settings.py"
 
 echo Running script
