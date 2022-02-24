f462dc3256e865494911f212949012ef53f3c389
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-02-24 17:43:37 +0100

    Update ui_settings.py

--- src/ui/ui_settings.py
+++ src/ui/ui_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Y:\Development\elmocut\exe\ui_settings.ui'
+# Form implementation generated from reading ui file 'C:\Users\kelmo\Desktop\OLD DESKTOP\-Python\CODE\elmocut\exe\ui_about.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
